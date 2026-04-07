55ccd04f0bc7e4874758b0d75e3f43ca6f651a34
thenicco <nicolas.miguel020605@gmail.com>
2026-04-07 15:56:36 -0300

    feat: adiciona pastas das aulas 24 e 25

---
+++ Aula24/index.js
@@ -0,0 +1,31 @@
+/* 
+Entre 0 - 11 = bom dia
+entre 12 - 17 = boa tarde
+entre 18 - 23 = boa noite
+
+
+sempre que utilizar else , precisa ter um if antes.
+// eu posso ter varios else ifs na checagem
+só posso ter um else na checagem
+
+*/
+
+// const hora = 412;
+
+// if (hora >= 0 && hora <= 11) {
+//     console.log('Bom dia');
+// } else if ( hora >= 12 && hora <= 17) {
+//     console.log("Boa tarde");
+// } else if (hora >= 18 && hora <=23 ){
+//     console.log('Boa noite')
+// } else {
+//     console.log('Hora invalida');
+// } 
+
+const tenhoGrana = 0;
+
+if(tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('N vou sair de casa');
+}
